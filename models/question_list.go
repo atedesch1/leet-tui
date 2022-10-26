9248c502d140047c6754cdc9cc896cc682191c34
atedesch1 <alvarotedeschi@gmail.com>
2022-10-26 11:33:37 -0300

    improve question_list code

--- models/question_list.go
+++ models/question_list.go
@@ -8,28 +8,60 @@ import (
 )
 
 type questionListModel struct {
-	cursor            int
 	totalNumQuestions int
 	questions         []api.QuestionInfo
 	skip              int
 	limit             int
 	categorySlug      string
+
+	cursor  int
+	loading bool
 }
 
 func newQuestionListModel() *questionListModel {
 	return &questionListModel{
-		cursor:            0,
 		totalNumQuestions: 0,
 		questions:         []api.QuestionInfo{},
 		skip:              0,
 		limit:             10,
 		categorySlug:      "",
+
+		cursor:  0,
+		loading: true,
 	}
 }
 
-func (m *questionListModel) getProblemsetQuestionList() tea.Msg {
+type problemsetQuestionListResponseMsg struct {
+	questionList api.ProblemsetQuestionList
+}
+
+func (m *questionListModel) getProblemsetQuestionListCmd() tea.Msg {
 	problemsetQuestionList, _ := api.GetProblemsetQuestionList(m.categorySlug, m.skip, m.limit)
-	return problemsetQuestionList
+	return problemsetQuestionListResponseMsg{
+		questionList: problemsetQuestionList,
+	}
+}
+
+func (m *questionListModel) goToPreviousPage() tea.Cmd {
+	if m.skip < m.limit {
+		return nil
+	}
+
+	m.skip -= m.limit
+	return nil
+}
+
+func (m *questionListModel) goToNextPage() tea.Cmd {
+	if m.skip+2*m.limit > m.totalNumQuestions {
+		return nil
+	}
+
+	m.skip += m.limit
+	if len(m.questions) < m.skip+m.limit {
+		m.loading = true
+		return m.getProblemsetQuestionListCmd
+	}
+	return nil
 }
 
 type SelectedQuestionMsg struct {
@@ -43,56 +75,58 @@ func (m *questionListModel) selectQuestionCmd() tea.Msg {
 }
 
 func (m *questionListModel) Init() tea.Cmd {
-	return m.getProblemsetQuestionList
+	return m.getProblemsetQuestionListCmd
 }
 
 func (m *questionListModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+
 	switch msg := msg.(type) {
 
-	case api.ProblemsetQuestionList:
-		m.totalNumQuestions = msg.Total
-		m.questions = append(m.questions, msg.Questions...)
+	case problemsetQuestionListResponseMsg:
+		m.loading = false
+		m.totalNumQuestions = msg.questionList.Total
+		m.questions = append(m.questions, msg.questionList.Questions...)
 
 	case tea.KeyMsg:
 		switch msg.String() {
 		case "up", "k":
 			if m.cursor > 0 {
 				m.cursor--
+			} else if m.cursor == 0 && m.skip >= m.limit {
+				cmd = m.goToPreviousPage()
+				m.cursor = m.limit - 1
 			}
 
 		case "down", "j":
-			if m.cursor < len(m.questions)-1 {
+			if m.cursor < m.limit-1 {
 				m.cursor++
+			} else if m.cursor == m.limit-1 && m.skip+2*m.limit <= m.totalNumQuestions {
+				cmd = m.goToNextPage()
+				m.cursor = 0
 			}
 
 		case "right", "h":
-			if m.skip+2*m.limit > m.totalNumQuestions {
-				break
-			}
-			m.skip += m.limit
-			if len(m.questions) < m.skip+m.limit {
-				return m, m.getProblemsetQuestionList
-			}
+			cmd = m.goToNextPage()
 
 		case "left", "l":
-			if m.skip >= 10 {
-				m.skip -= m.limit
-			}
+			cmd = m.goToPreviousPage()
 
 		case "enter", " ":
 			return m, m.selectQuestionCmd
 		}
 
 	}
-	return m, nil
+	return m, cmd
 }
 
 func (m *questionListModel) View() string {
-	if len(m.questions) < m.skip+m.limit {
-		return "Loading...\n\n"
-	}
+	s := "Choose a problem from below:\n\n"
 
-	s := "Choose the question!\n\n"
+	if m.loading {
+		s += "  Loading..."
+		return s
+	}
 
 	for i, choice := range m.questions[m.skip : m.skip+m.limit] {
 		cursor := " "
@@ -102,7 +136,7 @@ func (m *questionListModel) View() string {
 		s += fmt.Sprintf("%s %s\n", cursor, choice.Title)
 	}
 
-	s += fmt.Sprintf("\n\nPage %d/%d", m.skip/m.limit+1, m.totalNumQuestions/m.limit)
+	s += fmt.Sprintf("\nPage %d/%d", m.skip/m.limit+1, m.totalNumQuestions/m.limit)
 
 	return s
 }
